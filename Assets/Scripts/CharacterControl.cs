cf7fe4278994e67fbb437d5c9dc17f97afa5467b
Ved Rane <74463375+Get-DevVed-YT@users.noreply.github.com>
2023-03-06 09:39:13 -0500

    Rewrite entire server architecture to be server-authoritative + score system + physics and other stuff

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Diagnostics;
 using Unity.Netcode;
+using TMPro;
+using Unity.Collections;
 
 public class CharacterControl : NetworkBehaviour
 {
@@ -21,66 +23,195 @@ public class CharacterControl : NetworkBehaviour
     public Bow.PlayerBow playerBow;
     public Arrow.PlayerArrow playerArrow;
     public SpriteRenderer _renderer;
+    private float jumpForce = 10;
+    private float moveForce = 2;
+    GameObject[] players;
     Vector3 moveDirection;
+    [SerializeField] private TextMeshProUGUI playerName;
+    [SerializeField] private NetworkVariable<FixedString128Bytes> networkPlayerName = new NetworkVariable<FixedString128Bytes>("Player0", NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+    public NetworkVariable<int> losses = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+    public int los;
 
     private int rotationDirection;
 
-    void Start()
+    public override void OnNetworkSpawn()
     {
-        if (!IsOwner) return;
-        playerController = new PlayerController.Controller(characterRB, massBorder);
-        playerBow = new Bow.PlayerBow(center, bow, pivot);
+        playerName.text = networkPlayerName.Value.ToString();
+        NameClientRpc();
+    }
+
+    void HandleMovement()
+    {
+        if (IsLocalPlayer)
+        {
+            los = losses.Value / 2;
+            networkPlayerName.Value = "Player" + (OwnerClientId + 1) + ": " + losses.Value.ToString();
+            playerName.text = networkPlayerName.Value.ToString();
+            NameClientRpc();
+
+            if (pivot.transform.rotation.z >= -180 && pivot.transform.rotation.z < 180)
+            {
+                rotationDirection = -1;
+            }
+            else
+            {
+                rotationDirection = 1;
+            }
+
+            if (Input.GetKey(KeyCode.Q))
+            {
+                pivot.transform.RotateAround(transform.position, new Vector3(0, 0, -1 * rotationDirection), 300 * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.E))
+            {
+                pivot.transform.RotateAround(transform.position, new Vector3(0, 0, 1 * rotationDirection), 300 * Time.deltaTime);
+                //Debug.Log("rotating RIGHT");
+            }
+
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                characterRB.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+                characterRB.AddForce(Vector2.left * moveForce, ForceMode2D.Force);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+                characterRB.AddForce(Vector2.right * moveForce, ForceMode2D.Force);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!IsOwner) return;
+        los = losses.Value / 2;
+        if (IsServer && IsLocalPlayer)
+        {
+            HandleMovement();
+        } else
+        {
+            if (IsLocalPlayer)
+            {
+                NameServerRpc();
 
-        playerBow.updatePos();
+                if (pivot.transform.rotation.z >= -180 && pivot.transform.rotation.z < 180)
+                {
+                    NormalizeLeftServerRpc();
+                }
+                else
+                {
+                    NormalizeRightServerRpc();
+                }
 
-        if (playerBow.bow.transform.rotation.z >= -180 && playerBow.bow.transform.rotation.z < 180)
-        {
-            rotationDirection = -1;
-        }
-        else
-        {
-            rotationDirection = 1;
-        }
+                if (Input.GetKey(KeyCode.Q))
+                {
+                    ArrowLeftServerRpc();
+                }
+                else if (Input.GetKey(KeyCode.E))
+                {
+                    ArrowRightServerRpc();
+                }
 
-        if (Input.GetKey(KeyCode.Q))
-        {
-            playerBow.rotateBow(-1, rotationDirection);
-            //Debug.Log("rotating LEFT");
-        }
-        else if (Input.GetKey(KeyCode.E))
-        {
-            playerBow.rotateBow(1, rotationDirection);
-            //Debug.Log("rotating RIGHT");
-        }
-
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            playerController.moveUp();
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            transform.localRotation = Quaternion.Euler(0, 180, 0);
-            playerController.moveLeft();
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.localRotation = Quaternion.Euler(0, 0, 0);
-            playerController.moveRight();
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    UpServerRpc();
+                }
+                else if (Input.GetKey(KeyCode.A))
+                {
+                    LeftServerRpc();
+                }
+                else if (Input.GetKey(KeyCode.D))
+                {
+                    RightServerRpc();
+                }
+            }
         }
     }
 
-    void OnTriggerEnter(Collider attack)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if (attack.gameObject.tag == "Player")
+        if(collision.gameObject.tag == "Platform")
         {
-            moveDirection = characterRB.transform.position - attack.transform.position;
-            playerController.Knockback(moveDirection, attack);
+            WallServerRpc();
         }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void ArrowLeftServerRpc()
+    {
+        pivot.transform.RotateAround(transform.position, new Vector3(0, 0, -1 * rotationDirection), 300 * Time.deltaTime);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void ArrowRightServerRpc()
+    {
+        pivot.transform.RotateAround(transform.position, new Vector3(0, 0, 1 * rotationDirection), 300 * Time.deltaTime);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void UpServerRpc()
+    {
+        characterRB.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void LeftServerRpc()
+    {
+        transform.localRotation = Quaternion.Euler(0, 180, 0);
+        characterRB.AddForce(Vector2.left * moveForce, ForceMode2D.Force);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void RightServerRpc()
+    {
+        transform.localRotation = Quaternion.Euler(0, 0, 0);
+        characterRB.AddForce(Vector2.right * moveForce, ForceMode2D.Force);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void NormalizeLeftServerRpc()
+    {
+        rotationDirection = -1;
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void NormalizeRightServerRpc()
+    {
+        rotationDirection = 1;
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void WallServerRpc()
+    {
+        WallClientRpc();
+        losses.Value += 1;
+        transform.position = new Vector3(0f, 0f, 0f);
+    }
+
+    [ClientRpc]
+    private void WallClientRpc()
+    {
+        transform.position = new Vector3(0f, 0f, 0f);
+        networkPlayerName.Value = "Player" + (OwnerClientId + 1) + ": " + losses.Value.ToString();
+        playerName.text = networkPlayerName.Value.ToString();
+    }
+
+    [ServerRpc]
+    private void NameServerRpc()
+    {
+        los = losses.Value / 2;
+        networkPlayerName.Value = "Player" + (OwnerClientId + 1) + ": " + los.ToString();
+        playerName.text = networkPlayerName.Value.ToString();
+        NameClientRpc();
+    }
+
+    [ClientRpc]
+    private void NameClientRpc()
+    {
+        playerName.text = networkPlayerName.Value.ToString();
     }
 }
