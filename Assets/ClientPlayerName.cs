1720573d8d5657c9646bbd3e7a4d5004013742ae
Ved Rane <74463375+Get-DevVed-YT@users.noreply.github.com>
2023-03-06 09:35:22 -0500

    Rewrite entire server architecture to be server-authoritative + score system + physics and other stuff

--- Assets/ClientPlayerName.cs
+++ Assets/ClientPlayerName.cs
@@ -7,8 +7,6 @@ using Unity.Collections;
 
 public class ClientPlayerName : NetworkBehaviour
 {
-    [SerializeField] private TextMeshProUGUI playerName;
-    [SerializeField] private NetworkVariable<FixedString128Bytes> networkPlayerName = new NetworkVariable<FixedString128Bytes>("Player0", NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
 
     // Start is called before the first frame update
     void Start()
@@ -22,9 +20,4 @@ public class ClientPlayerName : NetworkBehaviour
         
     }
 
-    public override void OnNetworkSpawn()
-    {
-        networkPlayerName.Value = "Player" + (OwnerClientId + 1);
-        playerName.text = networkPlayerName.Value.ToString();
-    }
 }
